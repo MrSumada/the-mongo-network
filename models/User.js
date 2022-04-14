d59e8964f9eef324f8e562325ca3ff4bdf28559c
MrSumada <ricksamericana.com>
2022-04-14 02:00:41 -0400

    add inital files, User model

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+
+import { isEmail } from 'validator';
+
+const UserSchema = new Schema(
+    {
+        id: {
+            type: ObjectId
+        },
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: [ isEmail, 'invalid email']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+})
+
+const User = model('User', UserSchema);
+
+module.exports = User;
